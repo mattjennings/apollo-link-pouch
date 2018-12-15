cd06060d30a1d4f738f9f63e8ed03023898fa9ac
matt <mattjennings44@gmail.com>
2018-12-15 00:14:31 -0700

    update babel config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  verbose: false,
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.ts$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  globals: {
+    'ts-jest': {
+      tsConfig: './tsconfig.test.json',
+      babelConfig: true
+    }
+  }
 }
